5d79dc8a4fe7373602729cd499a42634a644182f
Amar P <63669768+amar2468@users.noreply.github.com>
2021-03-18 13:35:40 +0000

    Create FileProcessing.java
    Project that I picked is  - My Search Engine. I have just slowly started implementing some code. I have opened a random file and used the readFile method to read all the contents in the file. This is still very basic and is just for me to get used to working on the basic elements in this project.

---
+++ FileProcessing.java
@@ -0,0 +1,53 @@
+package com.javaapp.test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class FileProcessing 
+{
+	private String fileName;
+	private File document;
+	
+	public FileProcessing(String fileName)
+	{
+		this.setFileName(fileName);
+	}
+	
+	public void openFile()
+	{
+		document = new File(fileName);
+	}
+	
+	public void readFile()
+	{
+		String lineInFile = "";
+		
+		try
+		{
+			Scanner searchForWord = new Scanner(document);
+			
+			while(searchForWord.hasNextLine())
+			{
+				lineInFile = searchForWord.nextLine();
+				System.out.println(lineInFile);
+			}
+			searchForWord.close();
+		}
+		catch(FileNotFoundException e)
+		{
+			System.out.println("File not found: ");
+			e.printStackTrace();
+		}
+	}
+
+
+	private String getFileName() {
+		return fileName;
+	}
+
+	private void setFileName(String fileName) 
+	{
+		this.fileName = fileName;
+	}
+}
