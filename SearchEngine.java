809223514b4e2f805703ded95c75cb243b7d66ad
Amar P <63669768+amar2468@users.noreply.github.com>
2021-03-19 16:56:16 +0000

    Creating a new file called SearchEngine class
    Added this search engine java class. This class will allow the user to enter a word and search for it. It will have gui features.

--- SearchEngine.java
+++ SearchEngine.java
@@ -1,1 +1,23 @@
+package com.javaapp.test;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class SearchEngine extends JFrame
+{
+	JTextField searchForWords;
+	JButton searchButton;
+	JPanel panel;
+	
+	SearchEngine(String titleOfApp)
+	{
+		super(titleOfApp);
+		setSize(250,300);
+		
+	}
+	
+	
+}
 
