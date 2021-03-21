f2230686c66f91799103001562bfa24648038cac
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-03-21 15:38:03 +0000

    The control class was modified

--- Control.java
+++ Control.java
@@ -4,11 +4,6 @@ public class Control {
 
 	public static void main(String[] args) 
 	{
-		FileProcessing firstDocument = new FileProcessing("ap_docs2.txt");
-		
-		firstDocument.openFile();
-		firstDocument.readFile();
-		
 		
 		SearchEngine searchengine1 = new SearchEngine("My Search Engine");
 		System.out.println(searchengine1);
