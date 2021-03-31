a9fbb9086aec75939620b4cbf3d4ef0a19d1a72a
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-03-31 16:09:45 +0100

    Added comments in, Also, the object now prints the description of the program

--- Control.java
+++ Control.java
@@ -1,7 +1,7 @@
 /***********************************
 *Control: This is the control class which creates an object in the search engine class.
 *Author: Amar Plakalo
-*Date:30/03/2021
+*Date:31/03/2021
 ***********************************/
 
 
@@ -11,6 +11,7 @@ public class Control {
 
 	public static void main(String[] args) 
 	{
+		// Creating an object of the class Search Engine. The parameter inside will be the title of the GUI
 		
 		SearchEngine searchengine1 = new SearchEngine("My Search Engine");
 	
