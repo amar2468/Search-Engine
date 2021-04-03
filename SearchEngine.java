55d1fcc10a206dc7efb5c30eb1925c4974ebfee0
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-03 12:11:35 +0100

    Used entrySet to view my hash map and then a list in order to sort the values in descending order

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@
 *Search Engine: This is the search engine class which allows the user to enter the word they wish to find and the file processing
 *         class is called within this class so that the file can be opened and the word can be searched.
 *Author: Amar Plakalo
-*Date:31/03/2021
+*Date:03/04/2021
 ***********************************/
 
 
@@ -13,17 +13,22 @@ package com.javaapp.test;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+
 
 public class SearchEngine extends JFrame implements ActionListener
 {
@@ -34,7 +39,7 @@ public class SearchEngine extends JFrame implements ActionListener
 	SearchEngine(String titleOfApp)
 	{
 		super(titleOfApp);
-		setSize(250,300);
+		setSize(400,400);
 		panel = new JPanel();
 		
 		searchButton = new JButton("Search");
@@ -100,18 +105,29 @@ public class SearchEngine extends JFrame implements ActionListener
 			documentsToProcess.putAll(secondResult);
 			
 			
-			// Creating Linked Hash Map so I can sort the map by value in descending order. This allows me to 
-			// have the words that occur most to be placed at the top of the list
-			
-			LinkedHashMap<File, Integer> mostFrequentWordOccurence = new LinkedHashMap<>();
-			 
 
-			documentsToProcess.entrySet()
-			    .stream()
-			    .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder())) 
-			    .forEachOrdered(collect -> mostFrequentWordOccurence.put(collect.getKey(), collect.getValue()));
+			 // The entries inside the hash map will be viewed as an entrySet(). Entry set is needed when I need
+			 // both key and value from the data. In this case, I want to have the file name and the number of 
+	         	// times that the word occurred in that file	
+			 Set<Entry<File, Integer>> setContainingFilesWithOccurences = documentsToProcess.entrySet();
 			 
-			JOptionPane.showMessageDialog(this, mostFrequentWordOccurence);
+			 // An ArrayList is created because when using the Collections.sort method, the first parameter must be a list
+			 // so I had to make an array list out of the key,value.
+			 
+		     List<Entry<File, Integer>> listContainingFilesWithOccurences = new ArrayList<Entry<File, Integer>>(setContainingFilesWithOccurences);
+		     Collections.sort(listContainingFilesWithOccurences, new Comparator<Map.Entry<File, Integer>>()
+		     {
+		    	 public int compare(Map.Entry<File, Integer> firstOccurence, Map.Entry<File, Integer> secondOccurence)
+		         {
+		    		 return (secondOccurence.getValue()).compareTo(firstOccurence.getValue());
+		         }
+		     } 
+		     );
+		     
+			JOptionPane.showMessageDialog(this, listContainingFilesWithOccurences);
+			
+			
+			
 			
 			
 		
@@ -121,3 +137,4 @@ public class SearchEngine extends JFrame implements ActionListener
 	
 	
 }
+
