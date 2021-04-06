955c29fffb3d171df970f0e639140f1e1abd78bd
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-06 14:36:30 +0100

    text areas are created relative to the amount of files available. Also, utf8 is included to read files normally

--- ShowingFiles.java
+++ ShowingFiles.java
@@ -1,7 +1,7 @@
 /***********************************
 *Showing Files: This class shows the user the contents of the files they are using
 *Author: Amar Plakalo
-*Date:05/04/2021
+*Date:06/04/2021
 ***********************************/
 
 package com.javaapp.test;
@@ -15,28 +15,35 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextArea;
 
+
+@SuppressWarnings("serial")
 public class ShowingFiles extends JFrame
 {
-	private String fileNamesNeeded;
-	private File seeFileContents;
-	JTextArea showingContentsOfFiles;
+	private String[] fileNamesNeeded;
+	private File[] seeFileContents;
+	JTextArea[] showingContentsOfFiles;
 	JPanel newPanel;
 	
 	
-	ShowingFiles(String titleOfWindow,String fileNamesNeeded)
+	ShowingFiles(String titleOfWindow,String[] fileNamesNeeded)
 	{
 		super(titleOfWindow);
-		setSize(400,400);
-		
-		this.setFileNamesNeeded(fileNamesNeeded);
-	
-		
-		showingContentsOfFiles = new JTextArea(5,10);
-		
-		
+		setSize(1500,1500);
 		newPanel = new JPanel(new FlowLayout());
 		
-		newPanel.add(showingContentsOfFiles);
+		this.setFileNamesNeeded(fileNamesNeeded);
+		
+		seeFileContents = new File[fileNamesNeeded.length];
+		
+		showingContentsOfFiles = new JTextArea[fileNamesNeeded.length];
+	
+		for(int s = 0; s < fileNamesNeeded.length;s++)
+		{
+			showingContentsOfFiles[s] = new JTextArea(5,10);
+			newPanel.add(showingContentsOfFiles[s]);
+		}
+	
+
 		add(newPanel);
 		
 
@@ -48,26 +55,35 @@ public class ShowingFiles extends JFrame
 	
 	public void openingFilesNeeded()
 	{
-
-		seeFileContents = new File(getFileNamesNeeded());
+		for(int k = 0; k < fileNamesNeeded.length; k++)
+		{
+			seeFileContents[k] = new File(getFileNamesNeeded()[k]);
+		}
 	}
 	
 
-	public void readFileContents() 
+	public void readFileContents()
 	{
 		String holdingCurrentLine = "";
 		
 		try
 		{
-			Scanner s1 = new Scanner(seeFileContents);
-		
-			while (s1.hasNextLine())
+			for(int p = 0; p < fileNamesNeeded.length; p++)
 			{
-				holdingCurrentLine = s1.nextLine();
-				showingContentsOfFiles.append(holdingCurrentLine + "\n");
-			
+				Scanner s1 = new Scanner(seeFileContents[p],"UTF-8");
+				
+		
+				while (s1.hasNextLine())
+				{
+					holdingCurrentLine = s1.nextLine();
+					holdingCurrentLine = holdingCurrentLine.replaceAll("â€™","'");
+					showingContentsOfFiles[p].append(holdingCurrentLine + "\n");
+				
+				}
+				s1.close();
+
+
 			}
-			s1.close();
 		}
 		catch (FileNotFoundException e)
 		{
@@ -76,15 +92,15 @@ public class ShowingFiles extends JFrame
 		}
 	}
 
-	private String getFileNamesNeeded() {
+	private String[] getFileNamesNeeded() 
+	{
 		return fileNamesNeeded;
 	}
 
-	private void setFileNamesNeeded(String fileNamesNeeded) {
+	private void setFileNamesNeeded(String[] fileNamesNeeded) 
+	{
 		this.fileNamesNeeded = fileNamesNeeded;
 	}
 	
 
 }
-
-
