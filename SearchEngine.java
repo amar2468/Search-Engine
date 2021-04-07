79274c1eee9da5efb8774b8741ef71a339936225
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-07 16:24:10 +0100

    Used gridlayout, fully implemented the JFileChooser with error checking

--- SearchEngine.java
+++ SearchEngine.java
@@ -2,7 +2,7 @@
 *Search Engine: This is the search engine class which allows the user to enter the word they wish to find and the file processing
 *         class is called within this class so that the file can be opened and the word can be searched.
 *Author: Amar Plakalo
-*Date:06/04/2021
+*Date:07/04/2021
 ***********************************/
 
 
@@ -10,6 +10,9 @@ package com.javaapp.test;
 
 
 
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
@@ -18,7 +21,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
@@ -26,6 +28,7 @@ import java.util.Set;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
@@ -37,13 +40,26 @@ public class SearchEngine extends JFrame implements ActionListener
 {
 	JTextField searchForWords;
 	JButton searchButton,chooseFileToRead,showFiles;
+	JLabel title;
 	JPanel panel;
+	ArrayList<String> documents = new ArrayList<String>();
 	
 	SearchEngine(String titleOfApp)
 	{
 		super(titleOfApp);
-		setSize(600,600);
+		setSize(470,300);
 		panel = new JPanel();
+		panel.setBackground(Color.red);
+		
+		panel.setLayout(new GridLayout(1,4));
+		
+		FlowLayout flayout = new FlowLayout();
+		
+		JPanel panel2 = new JPanel();
+		panel2.setLayout(flayout);
+		panel2.setBackground(Color.gray);
+		
+		title = new JLabel("WELCOME TO THE SEARCH ENGINE! HAVE FUN!!!");
 		
 		searchButton = new JButton("Search");
 		showFiles = new JButton("Show Files");
@@ -59,13 +75,14 @@ public class SearchEngine extends JFrame implements ActionListener
 		
 		
 		
-		add(panel);
+		add(panel,"North");
+		add(panel2,"West");
 
-		
-		panel.add(searchForWords);
-		panel.add(searchButton);
-		panel.add(chooseFileToRead);
-		panel.add(showFiles);
+		panel.add(title);
+		panel2.add(searchForWords);
+		panel2.add(searchButton);
+		panel2.add(chooseFileToRead);
+		panel2.add(showFiles);
 		setVisible(true);
 		
 	}
@@ -82,66 +99,103 @@ public class SearchEngine extends JFrame implements ActionListener
 	{
 		if (eventDetected.getSource() == searchButton)
 		{
-			String [] documents = {"IndonesiaFloods.txt","Covid19News.txt","AFWLTeam.txt","IsraelGov.txt"};
-			FileProcessing documentsToRead = new FileProcessing(documents);
+			if(documents.isEmpty())
+			{
+				JOptionPane.showMessageDialog(this, "No files have been chosen. Please select files!");
+			}
+			else
+			{
+				FileProcessing documentsToRead = new FileProcessing(documents);
+				
+				documentsToRead.openFile();
+				
+				
+				String wordsTyped = searchForWords.getText();
+				
+				
+				String [] words = wordsTyped.split(" ");
+				
+				System.out.println(Arrays.toString(words));
+				
+				
+				Map<File, Integer> documentsToProcess = new HashMap<File, Integer>();
+				
+				Map<File, Integer> Results = new HashMap<File, Integer>();
+				
+		        
+				Results = documentsToRead.readFile(words);
 			
-			documentsToRead.openFile();
-			
-			
-			String wordsTyped = searchForWords.getText();
-			
-			
-			String [] words = wordsTyped.split(" ");
-			
-			System.out.println(Arrays.toString(words));
-			
-			
-			Map<File, Integer> documentsToProcess = new HashMap<File, Integer>();
-			
-			Map<File, Integer> Results = new HashMap<File, Integer>();
-			
-	        
-			Results = documentsToRead.readFile(words);
-		
-			
-			documentsToProcess.putAll(Results);
-			
-			
-
-			// The entries inside the hash map will be viewed as an entrySet(). Entry set is needed when I need
-			// both key and value from the data. In this case, I want to have the file name and the number of 
-	        	// times that the word occurred in that file	
-			Set<Entry<File, Integer>> setContainingFilesWithOccurences = documentsToProcess.entrySet();
-			 
-			// An ArrayList is created because when using the Collections.sort method, the first parameter must be a list
-			// so I had to make an array list out of the key,value.
-			 
-		    	List<Entry<File, Integer>> listContainingFilesWithOccurences = new ArrayList<Entry<File, Integer>>(setContainingFilesWithOccurences);
-		    	Collections.sort(listContainingFilesWithOccurences, new Comparator<Map.Entry<File, Integer>>()
-		    	{
-		    		public int compare(Map.Entry<File, Integer> firstOccurence, Map.Entry<File, Integer> secondOccurence)
-		        	{
-		    			return (secondOccurence.getValue()).compareTo(firstOccurence.getValue());
-		        	}
-		    	} 
-		    	);
-		     
-			JOptionPane.showMessageDialog(this, listContainingFilesWithOccurences);
-			
+				
+				documentsToProcess.putAll(Results);
+				
+				
+	
+				// The entries inside the hash map will be viewed as an entrySet(). Entry set is needed when I need
+				// both key and value from the data. In this case, I want to have the file name and the number of 
+		        	// times that the word occurred in that file	
+				Set<Entry<File, Integer>> setContainingFilesWithOccurences = documentsToProcess.entrySet();
+				 
+				// An ArrayList is created because when using the Collections.sort method, the first parameter must be a list
+				// so I had to make an array list out of the key,value.
+ 
+			    	ArrayList<Entry<File, Integer>> listContainingFilesWithOccurences = new ArrayList<Entry<File, Integer>>(setContainingFilesWithOccurences);
+			    	Collections.sort(listContainingFilesWithOccurences, new Comparator<Map.Entry<File, Integer>>()
+			    	{
+			    		public int compare(Map.Entry<File, Integer> firstOccurence, Map.Entry<File, Integer> secondOccurence)
+			        	{
+			    			return (secondOccurence.getValue()).compareTo(firstOccurence.getValue());
+			        	}
+			    	} 
+			    	);
+			     
+				JOptionPane.showMessageDialog(this, listContainingFilesWithOccurences);
+			}
+				
 		
 		}
 		
 		
 		else if(eventDetected.getSource() == chooseFileToRead)
 		{
+			int checker = 0;
 			JFileChooser j = new JFileChooser(FileSystemView.getFileSystemView());
 			  
 			int retVal = j.showSaveDialog(null);
 			
+			
 			if(retVal == JFileChooser.APPROVE_OPTION)
 			{
-				File fileChosen = j.getSelectedFile();
-				JOptionPane.showMessageDialog(this, "Successfully chose the file name " + fileChosen);
+				if(documents.isEmpty())
+				{
+					JOptionPane.showMessageDialog(this, "Successfully chose the file name " + j.getSelectedFile().getName());
+					documents.add(j.getSelectedFile().getName());
+					System.out.println(documents);	
+				}
+				else
+				{
+					for(int t=0; t < documents.size(); t++)
+					{
+						if(documents.get(t).equals(j.getSelectedFile().getName()))
+						{
+							checker = 1;
+							break;
+						}
+						
+					}
+					
+					if(checker == 1)
+					{
+						JOptionPane.showMessageDialog(this, "Already added the file");
+					}
+					else if(checker == 0)
+					{
+						JOptionPane.showMessageDialog(this, "Successfully chose the file name " + j.getSelectedFile().getName());
+						documents.add(j.getSelectedFile().getName());
+						System.out.println(documents);
+					}
+				}
+				
+				
 			}
 			else
 			{
@@ -152,11 +206,15 @@ public class SearchEngine extends JFrame implements ActionListener
 		
 		else if(eventDetected.getSource() == showFiles)
 		{
-			String [] documents = {"IndonesiaFloods.txt","Covid19News.txt","AFWLTeam.txt","IsraelGov.txt"};
 			ShowingFiles showingfiles1 = new ShowingFiles("Showing the Files",documents);
 			showingfiles1.openingFilesNeeded();
 			showingfiles1.readFileContents();
 		}
+
+	}
+	
+	
+}
 
 	}
 	
