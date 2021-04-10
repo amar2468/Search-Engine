46c14f407af3ca3a7755804b9e210f9964cdb726
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-10 15:49:41 +0100

    Added comments to the code

--- ShowingFiles.java
+++ ShowingFiles.java
@@ -1,7 +1,7 @@
 /***********************************
 *Showing Files: This class shows the user the contents of the files they are using
 *Author: Amar Plakalo
-*Date:07/04/2021
+*Date:10/04/2021
 ***********************************/
 
 package com.javaapp.test;
@@ -28,75 +28,90 @@ public class ShowingFiles extends JFrame
 	
 	ShowingFiles(String titleOfWindow,ArrayList<String> fileNamesNeeded)
 	{
+		// Inheriting the title and setting the size of the GUI window
 		super(titleOfWindow);
 		setSize(1500,1500);
+		
+		// Setting up the panel with a flow layout
 		newPanel = new JPanel(new FlowLayout());
 		
+		// Calling the setter and passing the file names
 		this.setFileNamesNeeded(fileNamesNeeded);
 		
+		// creating a file object and setting the size of it to be the size of the text file names array list
 		seeFileContents = new File[fileNamesNeeded.size()];
 		
+		// text area object with the same size as above.
 		showingContentsOfFiles = new JTextArea[fileNamesNeeded.size()];
 	
+		// for loop runs for the amount of files chosen
 		for(int s = 0; s < fileNamesNeeded.size();s++)
 		{
-			showingContentsOfFiles[s] = new JTextArea(5,10);
-			newPanel.add(showingContentsOfFiles[s]);
+			showingContentsOfFiles[s] = new JTextArea(5,10); // create a text area in that index
+			newPanel.add(showingContentsOfFiles[s]); // put the text area on the panel
 			
 		}
 	
 
-		add(newPanel);
+		add(newPanel); // add panel to screen
 		
 
-		setVisible(true);
+		setVisible(true); // makes everything visible on screen
 		
 		
 		
 	}
 	
-	public void openingFilesNeeded()
+	public void openingFilesNeeded() // method which allows user to open the file/files
 	{
-		for(int k = 0; k < fileNamesNeeded.size(); k++)
+		for(int k = 0; k < fileNamesNeeded.size(); k++) // loops for the amount of files chosen
 		{
-			seeFileContents[k] = new File(getFileNamesNeeded().get(k));
+			seeFileContents[k] = new File(getFileNamesNeeded().get(k)); // putting the filenames inside the file
+										   // object array list in the specific index
 		}
 	}
 	
 
-	public void readFileContents()
+	public void readFileContents() // allows the program to read the files and to present them on screen
 	{
-		String holdingCurrentLine = "";
+		String holdingCurrentLine = ""; // string variable that will hold the current line which will be appended
+		                               // to the textarea.
 		
-		try
+		try // attempt this first
 		{
-			for(int p = 0; p < fileNamesNeeded.size(); p++)
+			for(int p = 0; p < fileNamesNeeded.size(); p++) // iterates for the amount of files chosen
 			{
-				Scanner s1 = new Scanner(seeFileContents[p],"UTF-8");
+				Scanner s1 = new Scanner(seeFileContents[p],"UTF-8"); // scanner object scanning file in index p
 				
 		
-				while (s1.hasNextLine())
+				while (s1.hasNextLine()) // scanner scans the next line in the file
 				{
-					holdingCurrentLine = s1.nextLine();
-					showingContentsOfFiles[p].append(holdingCurrentLine + "\n");
+					holdingCurrentLine = s1.nextLine(); // put the line inside the string variable
+					showingContentsOfFiles[p].append(holdingCurrentLine + "\n"); // put the current line inside the
+												     // text area so that the full file
+												    // is read inside the text area
 				
 				}
-				s1.close();
+				s1.close(); // close scanner so the program can move on to the next file, if there are more files
 
 
 			}
 		}
-		catch (FileNotFoundException e)
+		catch (FileNotFoundException e) // if an exception occurred
 		{
-			System.out.println("File not found: ");
+			System.out.println("File not found: "); // print message
 			e.printStackTrace();
 		}
 	}
+	
+	// getter for file names
 
 	private ArrayList<String> getFileNamesNeeded() 
 	{
 		return fileNamesNeeded;
 	}
+	
+	// setter for file names
 
 	private void setFileNamesNeeded(ArrayList<String> fileNamesNeeded) 
 	{
@@ -104,4 +119,3 @@ public class ShowingFiles extends JFrame
 	}
 	
 
-}
