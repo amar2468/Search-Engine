fb92f316ed15dd8cb026f6c679df7ef021c99364
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-10 15:49:05 +0100

    Added the comments to describe parts of the code

--- FileProcessing.java
+++ FileProcessing.java
@@ -2,7 +2,7 @@
 *File Processing: This is the file processing class. This class allows the user to find which documents contain the word they typed
 *		  by opening the file and reading it. 
 *Author: Amar Plakalo
-*Date:07/04/2021
+*Date:10/04/2021
 ***********************************/
 
 
@@ -45,8 +45,8 @@ public class FileProcessing
 	
 	public Map<File, Integer> readFile(String [] wordsThatWereEntered)
 	{
-		// LineInFile string is used to store the line in the file so it can be checked against the search term/terms
-		String lineInFile = "";
+		// wordInFile string is used to store the line in the file so it can be checked against the search term/terms
+		String wordInFile = "";
 		
 		// Counter counts the amount of occurrences of a word in a file
 		int counter = 0;
@@ -56,76 +56,96 @@ public class FileProcessing
 		{
 			for(int m = 0; m < files.size(); m++)
 			{
+				// Scanner is created and it scans the file in the m index position of the files
+				
 				Scanner searchForWord = new Scanner(documentsNeeded[m],"UTF-8");
 			
 				
+				// while the file has another word i.e. while the file is still not fully read
+				
 				while(searchForWord.hasNext())
 				{
-					lineInFile = searchForWord.next();
+					wordInFile = searchForWord.next(); // store the next word inside the variable
 					
 
-					lineInFile = lineInFile.replaceAll("\\p{Punct}", "");
+					wordInFile = wordInFile.replaceAll("\\p{Punct}", ""); // remove punctuation from that word
 					
 				
-
+					// If the length of the word is greater than 1
+					
 					if (wordsThatWereEntered.length > 1)
 					{
+						// For loop runs for the amount of words that were entered
+						
 						for(int i = 0; i < wordsThatWereEntered.length; i++)
 						{
-							wordsThatWereEntered[i] = wordsThatWereEntered[i].replaceAll("\\p{Punct}", "");
-							if (lineInFile.equals(wordsThatWereEntered[i]))
+							wordsThatWereEntered[i] = wordsThatWereEntered[i].replaceAll("\\p{Punct}", ""); // remove punctuation from the word entered
+							if (wordInFile.equals(wordsThatWereEntered[i])) // if it is equal to the current word
+													// in the file
 							{
-								counter ++;
+								counter ++; // increment the counter
 							
 							}
 						}
 							
 						
 					}
+					// if only one word was entered into the textfield
 					else if(wordsThatWereEntered.length == 1)
 					{
+						// remove punctuation from the word
 						wordsThatWereEntered[0] = wordsThatWereEntered[0].replaceAll("\\p{Punct}", "");
-						if (lineInFile.equals(wordsThatWereEntered[0]))
+						if (wordInFile.equals(wordsThatWereEntered[0])) // if it is equal to the current word 
+												// in the file
 						{	
-							counter += 1;
+							counter += 1; // Increment the counter
 						}
 					}
 		
 				}
-				numberOfOccurences.put(documentsNeeded[m], counter);
-				counter = 0;
+				numberOfOccurences.put(documentsNeeded[m], counter); // put the current file and the amount
+										    // of occurrences of the words inside the map
+				counter = 0; // set counter to 0 so it can count new files
 			
 	
 				
 				
-				searchForWord.close();
+				searchForWord.close(); // close scanner so a new file can be read (if there are new files)
 			}
 		}
-		catch(FileNotFoundException e)
+		catch(FileNotFoundException e) // if the file is not found
 		{
-			System.out.println("File not found: ");
-			e.printStackTrace();
+			System.out.println("File not found: "); // print message
+			e.printStackTrace(); // used to handle exceptions in java
 		}
-		return numberOfOccurences;
+		return numberOfOccurences; // returns the map to the SearchEngine class so it can be sorted by value
 		
 	}
 
 
-	
-	private ArrayList<String> getFiles() {
+	// getter for files
+	private ArrayList<String> getFiles() 
+	{
 		return files;
 	}
 
+	// setter for files
 	private void setFiles(ArrayList<String> files) 
 	{
 		this.files = files;
 	}
+	
+	// Getter for number of occurrences
 
-	Map<File, Integer> getNumberOfOccurences() {
+	Map<File, Integer> getNumberOfOccurences() 
+	{
 		return numberOfOccurences;
 	}
+	
+	// setter for number of occurrences
 
-	void setNumberOfOccurences(Map<File, Integer> numberOfOccurences) {
+	void setNumberOfOccurences(Map<File, Integer> numberOfOccurences) 
+	{
 		this.numberOfOccurences = numberOfOccurences;
 	}
 }
