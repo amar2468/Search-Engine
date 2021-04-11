6f2b9ce58adc4814abe0a409d7490854115d182a
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-11 15:50:11 +0100

    No changes to this class

--- Control.java
+++ Control.java
@@ -1,7 +1,7 @@
 /***********************************
 *Control: This is the control class which creates an object in the search engine class.
 *Author: Amar Plakalo
-*Date:10/04/2021
+*Date:11/04/2021
 ***********************************/
 
  
@@ -12,11 +12,11 @@ public class Control
 
 	public static void main(String[] args) 
 	{
-		// Creating an object of the class Search Engine. The parameter inside will be the title of the GUI
+		// Creating an object of the class Search Engine. The parameter inside will be the title of the GUI.
 		
 		SearchEngine searchengine1 = new SearchEngine("My Search Engine - Amar Plakalo");
 	
-		// Print the object using the method toString() in that class
+		// Print the object using the method toString() in that class.
 		System.out.println(searchengine1);
 		
 		
