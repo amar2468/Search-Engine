d35e17d66dc82ff4c0a9213e0e91a4ad8a2ad57b
Amar Plakalo <c19387623@mytudublin.ieAmar.Plakalogit config --global user.name Amar.Plakalogit config --global user.email c19387623@mytudublin.ie>
2021-04-15 19:44:27 +0100

    Improved search engine so that it can detect whether term/terms are in the file. Error checking added

--- FileProcessing.java
+++ FileProcessing.java
@@ -2,7 +2,7 @@
 *File Processing: This is the file processing class. This class allows the user to find which documents contain the word they typed
 *		  by opening the file and reading it. 
 *Author: Amar Plakalo
-*Date:11/04/2021
+*Date:15/04/2021
 ***********************************/
 
 
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+
+
 import java.util.Scanner;
 
 
@@ -23,6 +25,8 @@ public class FileProcessing
 	private ArrayList <String> files;
 	private File[] documentsNeeded;
 	private Map<File, Integer> numberOfOccurences = new HashMap<File, Integer>();
+	private int var;
+	private int onlyOnce;
 	
 	public FileProcessing(ArrayList<String> files)
 	{
@@ -44,13 +48,21 @@ public class FileProcessing
 		}
 	}
 	
+	@SuppressWarnings("resource")
 	public Map<File, Integer> readFile(String [] wordsThatWereEntered)
 	{
 		// wordInFile string is used to store the line in the file so it can be checked against the search term/terms
 		String wordInFile = "";
+	
 		
 		// Counter counts the amount of occurrences of a word in a file
 		int counter = 0;
+		
+		int countMultipleWords = 0;
+		
+		var = 0;
+		
+		onlyOnce = 0;
 
 		
 		try
@@ -62,61 +74,117 @@ public class FileProcessing
 				Scanner searchForWord = new Scanner(documentsNeeded[m],"UTF-8");
 			
 			
-				
-				// while the file has another word i.e. while the file is still not fully read
-				
-				while(searchForWord.hasNext())
+				// If the length of the word is greater than 1
+				if (wordsThatWereEntered.length > 1)
 				{
-					wordInFile = searchForWord.next(); // store the next word inside the variable
-					
-				
-					wordInFile = wordInFile.replaceAll("\\p{Punct}", ""); // remove punctuation from that word
-					
-				
-					// If the length of the word is greater than 1
-					
-					if (wordsThatWereEntered.length > 1)
+						
+					// while the file has another word i.e. while the file is still not fully read
+						
+					while(searchForWord.hasNext())
 					{
+						wordInFile = searchForWord.next(); // store the next word inside the variable
+						
+						
+						wordInFile = wordInFile.replaceAll("\\p{Punct}", ""); // remove punctuation from that word
+						
 						// For loop runs for the amount of words that were entered
 						
 						for(int i = 0; i < wordsThatWereEntered.length; i++)
 						{
+								
 							wordsThatWereEntered[i] = wordsThatWereEntered[i].replaceAll("\\p{Punct}", ""); // remove punctuation from the word entered
-							if (wordInFile.equals(wordsThatWereEntered[i])) 
-							// if it is equal to the current word
-							// in the file
-							{
-								counter ++; // increment the counter
 							
+							
+							
+							if(wordsThatWereEntered.length == countMultipleWords)
+							{
+								var++;
+								numberOfOccurences.put(documentsNeeded[m], var);
+								return numberOfOccurences;
 							}
+							
+							// if it is equal to the current word in the file
+							
+							else if (wordInFile.equals(wordsThatWereEntered[i])) 
+							{
+								countMultipleWords ++; // increment the counter
+								
+								
+								if(wordsThatWereEntered.length == countMultipleWords)
+								{
+									var++;
+									numberOfOccurences.put(documentsNeeded[m], countMultipleWords);
+									return numberOfOccurences;
+								}
+								
+								else if(searchForWord.hasNext())
+								{
+								
+									wordInFile = searchForWord.next(); // store the next word inside the variable
+								
+								
+									wordInFile = wordInFile.replaceAll("\\p{Punct}", ""); // remove punctuation from that word
+									
+								}
+							
+								else
+								{
+									numberOfOccurences.put(documentsNeeded[m], countMultipleWords);
+									return numberOfOccurences;
+								}
+				
+							}
+							else
+							{
+								var = 0;
+								countMultipleWords = 0;
+								numberOfOccurences.put(documentsNeeded[m], countMultipleWords);
+								
+							}
+							
 						}
+						
+						
 							
 						
 					}
-					// if only one word was entered into the text field
-					else if(wordsThatWereEntered.length == 1)
+					searchForWord.close(); // close scanner so a new file can be read (if there are new files)
+				
+				}
+					
+				// if only one word was entered into the text field
+				else if(wordsThatWereEntered.length == 1)
+				{
+					while(searchForWord.hasNext())
 					{
-						// remove punctuation from the word
+						wordInFile = searchForWord.next(); // store the next word inside the variable
+							
+						
+						wordInFile = wordInFile.replaceAll("\\p{Punct}", ""); // remove punctuation from that word
+					
+
 						wordsThatWereEntered[0] = wordsThatWereEntered[0].replaceAll("\\p{Punct}", "");
 						if (wordInFile.equals(wordsThatWereEntered[0])) 
-					      	// if it is equal to the current word 
+						// if it is equal to the current word 
 						// in the file
 						{	
-							counter += 1; // Increment the counter
+							counter ++; // Increment the counter
+							onlyOnce = 1;
+							
+							
 						}
-					}
-		
-				}
-				
-				// put the current file and the amount
-				// of occurrences of the words inside the map
-				numberOfOccurences.put(documentsNeeded[m], counter); 
 
+					}
+					searchForWord.close();
+					numberOfOccurences.put(documentsNeeded[m], counter);
+				}
+	
+					
+	
+				countMultipleWords = 0;
 				counter = 0; // set counter to 0 so it can count new files
 	
-				
-				
-				searchForWord.close(); // close scanner so a new file can be read (if there are new files)
+
 			}
 		}
 		catch(FileNotFoundException e) // if the file is not found
@@ -124,13 +192,14 @@ public class FileProcessing
 			System.out.println("File not found: "); // print message
 			e.printStackTrace(); // used to handle exceptions in java
 		}
-		return numberOfOccurences; // returns the map to the SearchEngine class so it can be sorted by value
+		return numberOfOccurences;
 		
 	}
 	
-	public String calculatePercentage(Entry<File, Integer> entry,int b)
+	public String calculatePercentage(Entry<File, Integer> entry,int b, String[] userEntersWords)
 	{
 		float percentage = 0;
+		String formatPercentage = "";
 		@SuppressWarnings("unused")
 		int amountOfWords = 0;
 		
@@ -151,8 +220,23 @@ public class FileProcessing
 			e.printStackTrace(); // used to handle exceptions in java
 		}
 		
-		percentage = ((float)entry.getValue()) / amountOfWords * 100;
-		String formatPercentage = String.format("%.2f", percentage);
+		if(var >= 1 && onlyOnce == 0)
+		{
+			percentage = ((float)entry.getValue()) / amountOfWords * 100;
+			formatPercentage = String.format("%.2f", percentage);
+			
+		}
+		else if(var == 0 && onlyOnce == 1)
+		{
+			percentage = ((float)entry.getValue()) / amountOfWords * 100;
+			formatPercentage = String.format("%.2f", percentage);
+			
+		}
+		else if(var == 0 && onlyOnce == 0)
+		{
+			percentage = ((float)entry.getValue()) / amountOfWords * 100;
+			formatPercentage = String.format("%.2f", percentage);
+		}
 		return formatPercentage;
 	}
 
@@ -176,13 +260,6 @@ public class FileProcessing
 		return numberOfOccurences;
 	}
 	
-	// setter for number of occurrences
-
-	void setNumberOfOccurences(Map<File, Integer> numberOfOccurences) 
-	{
-		this.numberOfOccurences = numberOfOccurences;
-	}
-}
 	// setter for number of occurrences
 
 	void setNumberOfOccurences(Map<File, Integer> numberOfOccurences) 
