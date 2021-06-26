03d01b034635b93cabd26663e70e13aef400212c
Amar P <63669768+amar2468@users.noreply.github.com>
2021-06-26 19:21:32 +0100

    Uploaded the control.java file

--- Topic Modeller/Control.java
+++ Topic Modeller/Control.java
@@ -1,1 +1,36 @@
+package com.topicmodeller.test;
 
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
+public class Control 
+{
+
+	public static void main(String[] args) throws FileNotFoundException 
+	{
+		TopicModellerGUI topicmodeller = new TopicModellerGUI("Topic Modeller App - By Amar Plakalo");
+		
+		System.out.println(topicmodeller);
+		
+		ArrayList <String> fileNames = new ArrayList <String>();
+		
+		fileNames.add("text2.txt");
+		
+		fileNames.add("randomtextf.txt");
+		
+		WorkWithFiles setOfFiles = new WorkWithFiles(fileNames,"stopwords.txt");
+		
+		setOfFiles.openFile();
+
+		setOfFiles.openFile();
+		
+		setOfFiles.readStopWordFile();
+
+		setOfFiles.readFile();
+		
+		setOfFiles.checkWhetherDocumentsAreCommon();
+	
+
+	}
+
+}
