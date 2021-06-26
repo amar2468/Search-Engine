19e9bea1b2b98436a7b8c24cb33672ab1ddac491
Amar P <63669768+amar2468@users.noreply.github.com>
2021-06-26 19:22:53 +0100

    Uploaded the GUI file

---
+++ Topic Modeller/TopicModellerGUI.java
@@ -0,0 +1,78 @@
+package com.topicmodeller.test;
+
+import java.awt.Color;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class TopicModellerGUI extends JFrame implements ActionListener
+{
+	JButton button1;
+	
+	JPanel panel1,panel2;
+	
+	JLabel titleLabel;
+	
+	
+	TopicModellerGUI(String title)
+	{
+		super(title);
+		
+		setSize(400,340);
+		
+		panel1 = new JPanel();
+		panel1.setLayout(new FlowLayout());
+		panel1.setBackground(Color.pink);
+		
+		FlowLayout flay = new FlowLayout();
+		panel2 =  new JPanel();
+		panel2.setLayout(flay);
+		panel2.setBackground(Color.cyan);
+		
+		
+		button1 = new JButton("Check if same topic");
+		button1.setBackground(Color.green);
+		button1.addActionListener(this);
+		
+		
+		titleLabel = new JLabel("Enjoy using the Topic Modeller App!!");
+		titleLabel.setBackground(Color.darkGray);
+	
+		
+		add(panel1,"North");
+		add(panel2,"Center");
+		
+		panel1.add(titleLabel);
+		
+		panel2.add(button1);
+		
+		setVisible(true);
+		
+		
+	}
+	
+	public void actionPerformed(ActionEvent eventDetected)
+	{
+		if(eventDetected.getSource() == button1)
+		{
+			System.out.println("You pressed the check button!");
+			JOptionPane.showMessageDialog(button1, "You pressed the check button!");
+		}
+	}
+	
+	public String toString()
+	{
+		String stringresult;
+		
+		stringresult = "Enjoy using the Topic Modeller App!!";
+
+		return stringresult;
+	}
+
+}
