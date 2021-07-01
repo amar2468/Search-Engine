e19e5ba25ecc0fff42e71736433e0026399618b3
Amar P <63669768+amar2468@users.noreply.github.com>
2021-07-01 14:11:18 +0100

    Added in the filechooser (although I haven't fully implemented this)
    As mentioned in the title, I added a filechooser although I have not fully implemented it. Also, I added a button to choose a file and another button to see what words were saved from the files. This will be further improved.

--- Topic Modeller/TopicModellerGUI.java
+++ Topic Modeller/TopicModellerGUI.java
@@ -1,25 +1,44 @@
+/***********************************
+*TopicModellerGUI: This is the TopicModellerGUI class
+*Author: Amar Plakalo
+*Date:01/07/2021
+*/
+
 package com.topicmodeller.test;
 
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.filechooser.FileSystemView;
 
+@SuppressWarnings("serial")
 public class TopicModellerGUI extends JFrame implements ActionListener
 {
-	JButton button1;
+	JButton button1,button2,chooseFiles;
 	
 	JPanel panel1,panel2;
 	
 	JLabel titleLabel;
 	
 	String stringToBePrinted = "";
+	
+	private ArrayList<String>filesChosen = new ArrayList<String>();
+	
+	private Map<String,Integer>mapp2 = new HashMap<String,Integer>();
+	
+	private Map<String,Integer>test = new HashMap<String,Integer>();
+
 	
 	TopicModellerGUI(String title)
 	{
@@ -41,6 +60,13 @@ public class TopicModellerGUI extends JFrame implements ActionListener
 		button1.setBackground(Color.green);
 		button1.addActionListener(this);
 		
+		button2 = new JButton("Results");
+		button2.setBackground(Color.green);
+		button2.addActionListener(this);
+		
+		chooseFiles = new JButton("Choose File");
+		chooseFiles.setBackground(Color.green);
+		chooseFiles.addActionListener(this);
 		
 		titleLabel = new JLabel("Enjoy using the Topic Modeller App!!");
 		titleLabel.setBackground(Color.darkGray);
@@ -52,6 +78,8 @@ public class TopicModellerGUI extends JFrame implements ActionListener
 		panel1.add(titleLabel);
 		
 		panel2.add(button1);
+		panel2.add(button2);
+		panel2.add(chooseFiles);
 		
 		setVisible(true);
 		
@@ -62,12 +90,71 @@ public class TopicModellerGUI extends JFrame implements ActionListener
 		stringToBePrinted = resultFromCheck;
 	}
 	
+	public void receivingMaps(Map<String,Integer>mapp,Map<String,Integer>myMap)
+	{
+		mapp2.putAll(mapp);
+		System.out.println(mapp);
+		test.putAll(myMap);
+		System.out.println(myMap);
+	}
+	
 	public void actionPerformed(ActionEvent eventDetected)
 	{
 		if(eventDetected.getSource() == button1)
 		{
 			JOptionPane.showMessageDialog(button1, stringToBePrinted);
 			
+		}
+		
+		else if(eventDetected.getSource() == button2)
+		{
+			JOptionPane.showMessageDialog(button2, mapp2);
+			JOptionPane.showMessageDialog(button2, test);
+		}
+		
+		else if(eventDetected.getSource() == chooseFiles)
+		{
+			JFileChooser fileChooserWindow = new JFileChooser(FileSystemView.getFileSystemView());
+			
+			int checkIfFileAdded = 0;
+			
+			int value = fileChooserWindow.showSaveDialog(null);
+			
+			if(value == JFileChooser.APPROVE_OPTION)
+			{
+				if(filesChosen.isEmpty())
+				{
+					JOptionPane.showMessageDialog(this,"Successfully added a file");
+					filesChosen.add(fileChooserWindow.getSelectedFile().getName());
+				}
+				else
+				{
+					for(int f = 0; f < filesChosen.size(); f++)
+					{
+						if(filesChosen.get(f).equals(fileChooserWindow.getSelectedFile().getName()))
+						{
+							checkIfFileAdded = 1;
+							break;
+						}
+						
+					}
+					
+					if(checkIfFileAdded == 1)
+					{
+						JOptionPane.showMessageDialog(this,"File is already added");
+					}
+					
+					else
+					{
+						JOptionPane.showMessageDialog(this,"Successfully added filename - > " + fileChooserWindow.getSelectedFile().getName());
+						filesChosen.add(fileChooserWindow.getSelectedFile().getName());
+					}
+				}
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(this,"You have not added the file to this app!");
+			}
 		}
 	}
 	
@@ -81,4 +168,3 @@ public class TopicModellerGUI extends JFrame implements ActionListener
 	}
 
 }
-
