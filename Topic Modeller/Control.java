99edea805f27c7cdd00514978076e8f98b086c4d
Amar P <63669768+amar2468@users.noreply.github.com>
2021-07-04 15:34:23 +0100

    Deleted all objects and just let GUI object
    I moved the other objects into the other two classes so that the information can be shared between the two classes. I just left the TopicModellerGUI class object here so that a window opens with the title that was given in the argument.

--- Topic Modeller/Control.java
+++ Topic Modeller/Control.java
@@ -1,33 +1,18 @@
 /***********************************
 *Control: This is the control class
 *Author: Amar Plakalo
-*Date:01/07/2021
+*Date:04/07/2021
 */
 package com.topicmodeller.test;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
 
 public class Control 
 {
 	public static void main(String[] args) throws FileNotFoundException 
 	{
-		ArrayList <String> fileNames = new ArrayList <String>();
-		
-		fileNames.add("text2.txt");
-		
-		fileNames.add("randomtextf.txt");
-		
-		WorkWithFiles setOfFiles = new WorkWithFiles(fileNames,"stopwords.txt");
-		
-		setOfFiles.openFile();
-
-		setOfFiles.openFile();
-		
-		setOfFiles.readStopWordFile();
-
-		setOfFiles.readFile();
-
+		TopicModellerGUI obj = new TopicModellerGUI("Enjoy your app!!");
+		System.out.println(obj);
 	}
 
 }
