b95579e48f1380b0ce9094a87e730891f5ba1ea7
Amar P <63669768+amar2468@users.noreply.github.com>
2021-07-04 15:45:39 +0100

    Update WorkWithFiles.java
    Make quite a lot of changes in this class. I have two functions ( returnFirstMap() and returnSecondMap() ) which are used to sort the map by value in descending order. After this, the list is returned to the GUI class. In the function checkWhetherDocumentsAreCommon(), I used a nested for loop so I can compare each value from the first list with each value in the second list. If there was an occurrence, the occurrences variable was incremented and there was a break command that stopped the inner loop and jumped up to the outer for loop. Also changed the way you calculate the result. I took the top 10 words from both lists and divided the occurrences by 10 and multiplied by 100 to get a percentage. Note: The variable names in all classes will be changed. This was just used to get the program working the way it should.

--- Topic Modeller/WorkWithFiles.java
+++ Topic Modeller/WorkWithFiles.java
@@ -1,46 +1,50 @@
 /***********************************
 *WorkWithFiles: This is the WorkWithFiles class
 *Author: Amar Plakalo
-*Date:01/07/2021
+*Date:04/07/2021
 */
 
 package com.topicmodeller.test;
 
+import java.util.List;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.Map.Entry;
 
 public class WorkWithFiles 
 {
-	private ArrayList <String> textFileNames;
-	
 	private File[] filesUsed;
 	
-	private Map<String,Integer>wordsInFile;
-	
-	private File file;
+	private Map<String,Integer>wordsInFile = new HashMap<String,Integer>();
 	
 	private String fileName;
 	
+	private File file;
+	
 	private Map<String,Integer>secondMap;
+	
+	private ArrayList<String>textFileNames = new ArrayList<String>();
 
 	private ArrayList<String> allStopWords = new ArrayList<String>();
-	
 
-	WorkWithFiles(ArrayList<String> textFileNames,String fileName)
+
+	WorkWithFiles(ArrayList<String>textFileNames,String fileName)
 	{
-		this.textFileNames = textFileNames;
-		
-		this.fileName = fileName;
+		this.setTextFileNames(textFileNames);
+		this.setFileName(fileName);
 	}
 
 	
 	public void openFile()
 	{
-		filesUsed = new File[textFileNames.size()];
+		filesUsed = new File[getTextFileNames().size()];
 		
 		wordsInFile = new HashMap<String,Integer>();
 		
@@ -49,9 +53,9 @@ public class WorkWithFiles
 		file = new File(fileName);
 		
 		
-		for(int i = 0; i < textFileNames.size(); i++)
+		for(int i = 0; i < getTextFileNames().size(); i++)
 		{
-			filesUsed[i] = new File(textFileNames.get(i));
+			filesUsed[i] = new File(getTextFileNames().get(i));
 		}
 		
 		
@@ -66,7 +70,7 @@ public class WorkWithFiles
 		
 		try
 		{
-			for(int j = 0; j < textFileNames.size();j++)
+			for(int j = 0; j < getTextFileNames().size();j++)
 			{
 				Scanner scanFileContents = new Scanner(filesUsed[j],"UTF-8");
 				
@@ -75,8 +79,14 @@ public class WorkWithFiles
 				{
 					word = scanFileContents.next();
 					
-					
 					word = word.replace(",","");
+					
+					word = word.replaceAll("[^a-zA-Z0-9]","");
+					
+					word = word.replace(" ","");
+					
+					word = word.replaceAll("[-+^]*", "");  
+					
 					
 					word = word.replaceAll("\\p{Punct}", "");
 					
@@ -119,15 +129,6 @@ public class WorkWithFiles
 			
 				scanFileContents.close();
 			}
-			
-			TopicModellerGUI obj = new TopicModellerGUI("Enjoy using the Topic Modeller App!");
-			obj.receivingMaps(wordsInFile,secondMap);
-			
-			String checkResult = checkWhetherDocumentsAreCommon();
-
-			obj.initialiseVariableWithResultString(checkResult);
-			
-			
 		}
 		
 		catch (FileNotFoundException e)
@@ -136,91 +137,137 @@ public class WorkWithFiles
 			e.printStackTrace();
 		}
 		
+
+		
 	}
 	
-	public void readStopWordFile() throws FileNotFoundException
+	public List<Entry<String,Integer>> returnFirstMap()
+	{
+		Set<Entry<String,Integer>>set = wordsInFile.entrySet();
+		
+		List<Entry<String,Integer>>list = new ArrayList<Entry<String,Integer>>(set);
+		
+		Collections.sort(list,new Comparator<Map.Entry<String,Integer>>()
+		{
+			public int compare(Map.Entry<String,Integer>o1,Map.Entry<String,Integer>o2)
+			{
+				return o2.getValue().compareTo(o1.getValue());
+			}
+		});
+
+	
+		return list;
+	}
+	
+	
+	public List<Entry<String,Integer>> returnSecondMap()
+	{
+		Set<Entry<String,Integer>>set1 = secondMap.entrySet();
+		
+		List<Entry<String,Integer>>list1 = new ArrayList<Entry<String,Integer>>(set1);
+		
+		Collections.sort(list1,new Comparator<Map.Entry<String,Integer>>()
+		{
+			public int compare(Map.Entry<String,Integer>o3,Map.Entry<String,Integer>o4)
+			{
+				return o4.getValue().compareTo(o3.getValue());
+			}
+		});
+				
+		return list1;
+	}
+	
+	public void readStopWordFile()
 	{
 		String currentStopWord = "";
 		
-		Scanner scanCurrentStopWord = new Scanner(file);
-		
-		while(scanCurrentStopWord.hasNextLine())
+		try
 		{
-			currentStopWord = scanCurrentStopWord.next();
-			
-			currentStopWord = currentStopWord.replace(",","");
-			
-			currentStopWord = currentStopWord.replaceAll("\\p{Punct}", "");
-			
-			allStopWords.add(currentStopWord);
-			
-		}
-	
 		
-		
-		scanCurrentStopWord.close();
-
-	}
-	
-	public String checkWhetherDocumentsAreCommon()
-	{
-		int counter = 0;
-		
-		for(String key:wordsInFile.keySet())
-		{
-			if(secondMap.containsKey(key))
+			Scanner scanCurrentStopWord = new Scanner(file);
+			
+			while(scanCurrentStopWord.hasNextLine())
 			{
-				counter += 1;
+				currentStopWord = scanCurrentStopWord.next();
+				
+				currentStopWord = currentStopWord.replace(",","");
+				
+				currentStopWord = currentStopWord.replaceAll("\\p{Punct}", "");
+				
+				allStopWords.add(currentStopWord);
+				
 			}
+		
+			scanCurrentStopWord.close();
 		}
 		
-		System.out.println("Amount of overlaps - > " + counter);
-		
-		
-		int totalAmountOfWords = 0;
-		
-		if(wordsInFile.size() > secondMap.size())
+		catch (FileNotFoundException e)
 		{
-			totalAmountOfWords = wordsInFile.size();
+			System.out.println("Could not find file");
+			e.printStackTrace();
 		}
+	}
+	
+	public String checkWhetherDocumentsAreCommon(List<Entry<String, Integer>> a1,List<Entry<String, Integer>> a2)
+	{
+		int occurrences = 0;
 		
-		else if(wordsInFile.size() == secondMap.size())
+		String returnStringResult = "";
+	
+		for(Entry<String,Integer>element:a1)
 		{
-			totalAmountOfWords = wordsInFile.size();
+			for(Entry<String,Integer>element2:a2)
+			{
+				if(element.getKey().equals(element2.getKey()))
+				{
+					occurrences += 1;
+					break;
+				}
+			}
+			
 		}
 		
-		else if(wordsInFile.size() < secondMap.size())
+	
+		System.out.println("Amount of overlaps - > " + occurrences);
+	
+		
+		if((((float)occurrences / (float)10)*100) >= 70)
 		{
-			totalAmountOfWords = secondMap.size();
+			returnStringResult =  "Likely to be about the same topic";
 		}
 		
-		System.out.println("Total amount of words - > " + totalAmountOfWords);
-		
-		
-		if((((float)counter / (float)totalAmountOfWords)*100) >= 70)
+		else if((((float)occurrences / (float)10)*100) > 40 && ((occurrences / 10) * 100) < 70)
 		{
-			return "Likely to be about the same topic";
+			returnStringResult = "Not sure whether they are about the same topic";
+		}
+		else if((((float)occurrences / (float)10)*100) <= 40)
+		{
+			returnStringResult = "Not likely to be about the same topic";
 		}
 		
-		else if((((float)counter / (float)totalAmountOfWords)*100) > 40 && ((counter / totalAmountOfWords) * 100) < 70)
-		{
-			return "Not sure whether they are about the same topic";
-		}
-		else if((((float)counter / (float)totalAmountOfWords)*100) <= 40)
-		{
-			return "Not likely to be about the same topic";
-		}
-		
-		return "Nothing";
+		return returnStringResult;
 	}
 
-	public String getFile() {
+
+	private ArrayList<String> getTextFileNames() {
+		return textFileNames;
+	}
+
+
+	private void setTextFileNames(ArrayList<String> textFileNames) {
+		this.textFileNames = textFileNames;
+	}
+
+
+	private String getFileName() {
 		return fileName;
 	}
 
-	public void setFile(String fileName2) {
-		this.fileName = fileName2;
+
+	private void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
+
 
 
 }
