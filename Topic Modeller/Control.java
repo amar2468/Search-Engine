efaa0f1b50c5ad8806baf029d79e8d6cae152ff1
Amar P <63669768+amar2468@users.noreply.github.com>
2021-07-05 11:33:44 +0100

    Updated the class description

--- Topic Modeller/Control.java
+++ Topic Modeller/Control.java
@@ -1,7 +1,8 @@
 /***********************************
-*Control: This is the control class
+*Control: This is the control class. This class calls the TopicModellerGUI constructor and passes in the title
+*of the GUI window.
 *Author: Amar Plakalo
-*Date:04/07/2021
+*Date:05/07/2021
 */
 package com.topicmodeller.test;
 
