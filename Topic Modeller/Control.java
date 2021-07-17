456d13fa9ff7fa386f0c7408a8873e8aeae536d3
Amar P <63669768+amar2468@users.noreply.github.com>
2021-07-17 11:40:34 +0100

    Commented this class

--- Topic Modeller/Control.java
+++ Topic Modeller/Control.java
@@ -2,7 +2,7 @@
 *Control: This is the control class. This class calls the TopicModellerGUI constructor and passes in the title
 *of the GUI window.
 *Author: Amar Plakalo
-*Date:10/07/2021
+*Date:17/07/2021
 */
 package com.topicmodeller.test;
 
@@ -12,6 +12,8 @@ public class Control
 {
 	public static void main(String[] args) throws FileNotFoundException 
 	{
+		// Created GUI object that will open a window and have the title as the one below
+		
 		TopicModellerGUI obj = new TopicModellerGUI("Enjoy your app!!");
 		System.out.println(obj);
 	}
