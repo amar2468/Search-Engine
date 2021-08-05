dbc1e5c3384ccdc2985edfa9e29396f7709a3e1b
Amar P <63669768+amar2468@users.noreply.github.com>
2021-08-05 15:20:49 +0100

    Made more ResultSet and Statement objects
    I made more ResultSet and Statement objects to use in the other class. This was because I have to create separate statement objects for each query I write. Then, I also added a program description

--- Data Modeller/Control.java
+++ Data Modeller/Control.java
@@ -1,3 +1,10 @@
+/***********************************
+*Control: In this class, I created a connection with the database which I used and created some statements which
+*I would use in the other class
+*Author: Amar Plakalo
+*Date:05/08/2021
+*/
+
 package com.datamodeller.test;
 
 import java.sql.*;
@@ -15,11 +22,19 @@ public class Control
 		
 		Statement st1 = conn.createStatement();
 		
+		Statement st2 = conn.createStatement();
+		
+		Statement st3 = conn.createStatement();
+		
 		ResultSet rs = st.executeQuery("SELECT * from UnemploymentLevels");
 		
 		ResultSet rs_female = st1.executeQuery("SELECT * from UnemploymentLevels");
 		
-		DataModellerGUI obj = new DataModellerGUI(st,rs,rs_female);
+		ResultSet rs_male = st2.executeQuery("SELECT * from UnemploymentLevels");
+		
+		ResultSet rs_male_2 = st3.executeQuery("SELECT * from UnemploymentLevels");
+		
+		DataModellerGUI obj = new DataModellerGUI(st,rs,rs_female,rs_male,rs_male_2);
 		
 		System.out.println(obj);
 		
